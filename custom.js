8787cfb20543b06ab0b6ebfd9d718f62d7d89ab6
RainerZarth <rainer.zarth@dilewe.de>
2022-05-04 14:20:43 +0200

    timeline, findthe hotspot, agamotto and translation hotfixes

--- custom.js
+++ custom.js
@@ -1,1 +1,19 @@
-//console.log("mpub h5p");
+const hotSpotReplace = () => {
+	const textOf = document.querySelector(".h5p-question-feedback-content-text");
+	textOf.innerText = textOf.innerText.replace(" of ", " von ");
+}
+
+const hotSpotFix = () => {
+	const wrapper = document.querySelector(".image-hotspot-question .image-wrapper");
+	if (!wrapper) { return;}
+	const observeWrapper = new MutationObserver( mutations => {
+		mutations.forEach(mutation => {
+			if (mutation.type === "childList") {
+				hotSpotReplace();
+			}
+		})
+	})
+	observeWrapper.observe(wrapper, { childList: true });
+}
+
+window.addEventListener("load", hotSpotFix);
