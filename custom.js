d0cf96ddb95bc8dd79d4880d2c6c643872f025e6
RainerZarth <rainer.zarth@dilewe.de>
2023-03-08 15:57:41 +0100

    added a fullscreen option for all elements without fullscreen

--- custom.js
+++ custom.js
@@ -16,7 +16,42 @@ const hotSpotFix = () => {
 	observeWrapper.observe(wrapper, { childList: true });
 }
 
+const openFullScreen = elem => {
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) {
+    	elem.msRequestFullscreen();
+  	}
+}
+
+const FullScreenToggler = () => {
+	const currentEnv = document.body.classList;
+	const h5pContainer = document.querySelector(".h5p-container.h5p-standalone");
+	const hasControls = Boolean(h5pContainer.querySelector(".h5p-content-controls"));
+	if (!hasControls) {
+		//build the toggler like the h5p integrated one to look same
+		const cControls = document.createElement("div");
+		cControls.classList.add("h5p-content-controls");
+		const btnFullscreen = document.createElement("div");
+		btnFullscreen.classList.add("h5p-enable-fullscreen");
+		btnFullscreen.setAttribute("role", "button");
+		btnFullscreen.setAttribute("tabindex", "0");
+		btnFullscreen.setAttribute("aria-label", "Fullscreen");
+		btnFullscreen.setAttribute("title", "Fullscreen");
+		cControls.appendChild(btnFullscreen);
+		h5pContainer.appendChild(cControls);
+		btnFullscreen.onclick = e => {
+			e.preventDefault();
+			e.stopPropagation();
+			openFullScreen(h5pContainer);
+		}
+	}
+}
+
 window.addEventListener("load", hotSpotFix);
+window.addEventListener("load", FullScreenToggler);
 
 const referrerToClass = ref => {
 	const refClassMapping = {
@@ -27,6 +62,7 @@ const referrerToClass = ref => {
 		"redaktionsvorschau.lasub.dilewe.de": "lasub",
 		"module-sachsen.dilewe.de": "lasub",
 		"lasub.staging.test-dilewe.de": "lasub",
+
 	};
 	return refClassMapping[ref] || "unknown";
 };
